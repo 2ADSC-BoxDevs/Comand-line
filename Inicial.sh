dfe9eca6cbfb79b3946857fe72bc0c09e0f5f846
Matheus <matheus.gregorin@sptech.school>
2022-10-24 19:54:50 -0300

    atualizado

--- Inicial.sh
+++ Inicial.sh
@@ -4,5 +4,4 @@ sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker pull mysql:5.7
-sudo docker run -d -p 3306:3306 --name ConteinerBD -e "MYSQL_DATABASE=Specula" -e
-"MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
+sudo docker run -d -p 3306:3306 --name ConteinerBD -e "MYSQL_DATABASE=Specula" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
