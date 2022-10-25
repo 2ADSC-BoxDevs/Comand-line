f524efc8d950da59b833308078d822f58f1bd655
Matheus <matheus.gregorin@sptech.school>
2022-10-25 11:18:37 -0300

    Comando pré atualizado, faltando java

--- ComandoInicial.sh
+++ ComandoInicial.sh
@@ -10,16 +10,13 @@ sudo systemctl enable docker
 //Puxando imagem do mysql na VM
 
 sudo docker pull mysql:5.7
-sudo docker run -d -p 3306:3306 --name ConteinerBD -e "MYSQL_DATABASE=Specula" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
+sudo docker run -d -p 3306:3306 --name Specula -e "MYSQL_DATABASE=specula" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
 
 // aṕos isso
-
-// sudo Docker ps // 
-para verificar se o docker está rodando
-
-// sudo docker exec -it idDoContainer bash // 
-para entrar no terminal do Container
-
-//mysql -u root -p //
-para entrar no mysql do docker e lá criar o database, table e inserts.
+// para verificar se o docker está rodando
+// sudo Docker ps
+// para entrar no terminal do Container
+// sudo docker exec -it idDoContainer bash
+// para entrar no mysql do docker e lá criar o database, table e inserts.
+// mysql -u root -p
 
