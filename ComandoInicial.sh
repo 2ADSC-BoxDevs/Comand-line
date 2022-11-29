6033b5d1d050b4ae0cae2786aa9371f2bf7b2f0e
JoaoLeka <joao.barbosa@sptech.school>
2022-11-29 12:39:23 -0300

    subindo docker java e mysql

--- ComandoInicial.sh
+++ ComandoInicial.sh
@@ -15,15 +15,20 @@ if [ $? -eq 0 ]
    echo "$(tput setaf 10)[Bot SPECULA assistant]:$(tput setaf 7) Você não tem o JAVA instalado..."
    sleep 2 
    echo "$(tput setaf 10)[Bot SPECULA assistant]:$(tput setaf 7) Vamos instalar ele para você..."
-  
-   sudo docker build -t my-java-app .
+   sudo docker build -t specula-java -f ./docker_java/Dockerfile  .
 
-   sudo docker run -it --rm --name my-running-app my-java-app
+   sudo docker run -it --rm --name specula-java specula
+
+   
 
    sleep 2
    echo "$(tput setaf 10)[Bot SPECULA assistant]:$(tput setaf 7) JAVA instalado!"
    fi
    sleep 2
+
+  echo "$(tput setaf 10)[Bot SPECULA assistant]:$(tput setaf 7) Clonando repositório!"
+  git clone https://github.com/2ADSC-BoxDevs/aplicacao-java.git
+
   echo "$(tput setaf 10)[Bot SPECULA assistant]:$(tput setaf 7) Vamos instalar o DOCKER também..."
    sudo apt install docker.io
    sudo systemctl start docker
@@ -31,13 +36,14 @@ if [ $? -eq 0 ]
    sleep 2
   echo "$(tput setaf 10)[Bot SPECULA assistant]:$(tput setaf 7) Vamos instalar a imagem do MYSQL..."
    sleep 2
+sudo docker network create "Specula"
+sudo docker build -t  specula -f ./DockerfileBD .
+sudo docker run --network "specula" -d -p 3306:3306 --name Specula -e MYSQL_ROOT_PASSWORD=urubu100 -e MYSQL_DATABASE=specula specula
+echo "$(tput setaf 10)[Bot SPECULA assistant]:$(tput setaf 7) Rodando aplicação"
+cd..
+cd aplicacao-java
+chmod 777 -R aplicacao-specula-1.0-SNAPSHOT-jar-with-dependencies.jar 
+java -jar aplicacao-specula-1.0-SNAPSHOT-jar-with-dependencies.jar
 
-   sudo docker network create "Specula"
-
-  sudo docker build -t  specula  .
-  
- 
-
- sudo docker run --network "specula" -d -p 3306:3306 --name Specula -e MYSQL_ROOT_PASSWORD=urubu100 -e MYSQL_DATABASE=specula specula
 
 
