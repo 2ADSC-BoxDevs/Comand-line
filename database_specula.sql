5efab84972837ceb16bd30a02e1127535b24cc83
JoaoLeka <joao.barbosa@sptech.school>
2022-11-29 16:17:21 -0300

    atualizando

--- database_specula.sql
+++ database_specula.sql
@@ -10,9 +10,5 @@ create table historico_maquina (
     processador_em_uso varchar(30),
     disco_em_uso varchar(30),
     data_hora_registro datetime default current_timestamp,
-<<<<<<< HEAD
     discoEmUso varchar(30)
-=======
-    discoEmUso varchar(40)
->>>>>>> 6033b5d1d050b4ae0cae2786aa9371f2bf7b2f0e
 );
